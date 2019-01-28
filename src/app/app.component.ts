67c47016065a6958ac6c99b79815f5c7049ea88c
coolinich <coolinich@gmail.com>
2019-01-28 22:46:36 +0200

    remove extra code (from classwork)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesson5';
-  name = 'Some name';
-  isActive = true;
-  constructor() {
-    setTimeout(() => {
-      // this.name = 'new value';
-      this.isActive = false;
-    }, 2000);
-  }
-  onNameChangeHandler(value) {
-    console.log(value);
-  }
+  constructor() { }
 }
